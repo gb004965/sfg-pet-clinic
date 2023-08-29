49677ece899213b033a09066e80c8ae3b84e9fb7
Rory O'Connor <gonzooo@gmail.com>
2023-08-29 16:13:31 +1000

    Add Specialities to Vets, closes #34

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+import guru.springframework.sfgpetclinic.model.Speciality;
 import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.SpecialtyService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -26,6 +34,17 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if (object.getSpeciality().size() > 0) {
+            object.getSpeciality().forEach(speciality -> {
+                if (speciality != null) {
+                    if (speciality.getId() == null) {
+                        Speciality returnedSpeciality = specialtyService.save(speciality);
+                        speciality.setId(returnedSpeciality.getId());
+                    }
+                }
+            });
+        }
+
         return super.save(object);
     }
 
