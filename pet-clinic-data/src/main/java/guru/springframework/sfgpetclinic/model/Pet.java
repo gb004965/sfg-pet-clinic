f9311649203e8a55532b7ff422dedaf9aa146fae
Rory O'Connor <gonzooo@gmail.com>
2023-09-13 21:14:54 +1000

    Create Visit entity & mappings, closes #38

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -3,6 +3,8 @@ package guru.springframework.sfgpetclinic.model;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -21,6 +23,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visit = new HashSet<>();
 
     public String getName() {
         return name;
@@ -52,5 +57,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birtDate) {
         this.birthDate = birtDate;
+    }
+
+    public Set<Visit> getVisit() {
+        return visit;
+    }
+
+    public void setVisit(Set<Visit> visit) {
+        this.visit = visit;
     }
 }
